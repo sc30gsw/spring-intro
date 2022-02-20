bfb05bc31ad74cb49d3d2c172fe85a89b6185b24
sc30gsw <haidoushi875@gmail.com>
2022-02-20 11:15:30 +0900

    first commit

---
+++ echo-app/src/main/java/com/example/demo/EchoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoAppApplication.class, args);
+	}
+
+}
