4714c624d030aa935ee70cc19f6634a2919ad1b7
sc30gsw <haidoushi875@gmail.com>
2022-02-23 10:18:08 +0900

    UriComponentsBuilderを利用したURIの作成

--- rest-book/src/main/java/com/example/demo/controller/BookController.java
+++ rest-book/src/main/java/com/example/demo/controller/BookController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.example.demo.domain.entity.Book;
 import com.example.demo.domain.entity.BookCriteria;
@@ -34,66 +34,73 @@ public class BookController {
 
 	@Autowired
 	private BookService bookService;
-	
+
 	@CrossOrigin(maxAge = 900)
 	@GetMapping("/{bookId}")
 	public BookResource getBook(@PathVariable String bookId) {
-		
+
 		Book book = bookService.find(bookId);
-		
-		if(book == null) {
+
+		if (book == null) {
 			throw new BookResourceNotFoundException(bookId);
 		}
-		
+
 		BookResource resource = new BookResource();
 		resource.setBookId(book.getBookId());
 		resource.setName(book.getName());
 		resource.setPublishedDate(book.getPublishedDate());
-		
+
 		return resource;
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<Void> createBook(@Validated @RequestBody BookResource newResource) {
-		
+	public ResponseEntity<Void> createBook(@Validated @RequestBody BookResource newResource,
+			UriComponentsBuilder uriBuilder) {
+
 		Book newBook = new Book();
 		newBook.setName(newResource.getName());
 		newBook.setPublishedDate(newResource.getPublishedDate());
-		
+
 		Book createdBook = bookService.create(newBook);
-		
-		String resourceUri = "http://localhost:8080/books/" + createdBook.getBookId();
-		
-		return ResponseEntity.created(URI.create(resourceUri)).build();
+
+		// String resourceUri = "http://localhost:8080/books/" + createdBook.getBookId();
+		URI resourceUri = uriBuilder
+				.path("books/{bookId}")
+				.buildAndExpand(createdBook.getBookId())
+				.encode()
+				.toUri();
+
+		//return ResponseEntity.created(URI.create(resourceUri)).build();
+		return ResponseEntity.created(resourceUri).build();
 	}
-	
+
 	@PutMapping("/{bookId}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void put(@PathVariable String bookId, @Validated @RequestBody BookResource resource) {
-		
+
 		Book book = new Book();
 		book.setBookId(bookId);
 		book.setName(resource.getName());
 		book.setPublishedDate(resource.getPublishedDate());
-		
+
 		bookService.update(book);
 	}
-	
+
 	@DeleteMapping("/{bookId}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delte(@PathVariable String bookId) {
 		bookService.delete(bookId);
 	}
-	
+
 	@GetMapping
 	public List<BookResource> searchBooks(@Validated BookResourceQuery query) {
-		
+
 		BookCriteria criteria = new BookCriteria();
 		criteria.setName(query.getName());
 		criteria.setPublishedDate(query.getPublishedDate());
-		
+
 		List<Book> books = bookService.findAllByCriteria(criteria);
-		
+
 		return books.stream().map(book -> {
 			BookResource resource = new BookResource();
 			resource.setBookId(book.getBookId());
