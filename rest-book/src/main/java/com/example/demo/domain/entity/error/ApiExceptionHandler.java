4daf5dd591667ff21fb21fe1f6f3b76b29b56647
sc30gsw <haidoushi875@gmail.com>
2022-02-23 12:44:43 +0900

    エラーメッセージの変換

--- rest-book/src/main/java/com/example/demo/domain/entity/error/ApiExceptionHandler.java
+++ rest-book/src/main/java/com/example/demo/domain/entity/error/ApiExceptionHandler.java
@@ -1,18 +1,34 @@
 package com.example.demo.domain.entity.error;
 
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+	
+	private final Map<Class<? extends Exception>, String> messageMappings =
+			Collections.unmodifiableMap(new LinkedHashMap() {{
+				put(HttpMessageNotReadableException.class, "Request body is invalid");
+			}});
+	
+	private String resolveMessage(Exception e, String defaultMessage) {
+		return messageMappings.entrySet().stream()
+				.filter(entry -> entry.getKey().isAssignableFrom(e.getClass())).findFirst()
+				.map(Map.Entry::getValue).orElse(defaultMessage);
+	}
 
 	private ApiError createApiError(Exception e) {
 		ApiError apiError = new ApiError();
-		apiError.setMessage(e.getMessage());
+		apiError.setMessage(resolveMessage(e, e.getMessage()));
 		apiError.setDocumentationUrl("http://example.com/api/errors");
 		return apiError;
 	}
