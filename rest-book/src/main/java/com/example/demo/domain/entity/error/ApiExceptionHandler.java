bd52bc8c6362f915c154182f53d57dbf722e2537
sc30gsw <haidoushi875@gmail.com>
2022-02-23 12:21:39 +0900

    エラー情報をレスポンスボディに出力するための実装
    サポートしていないHTTPメソッドにリクエストするとエラー情報がレスポンスとして出力される
    
    ■Curlコマンド例
    curl -D - -H "Content-type: application/json" -X PUT -d '{"name":"Spring徹底入門", "publishedDate":"2016-04-01"}' http://localhost:8080/books
    
    ■出力例
    HTTP/1.1 405
    Allow: GET,POST
    
    ■JSON例
    {"message":"Request method 'PUT' not supported","documentaion_url":"http://example.com/api/errors"}

--- rest-book/src/main/java/com/example/demo/domain/entity/error/ApiExceptionHandler.java
+++ rest-book/src/main/java/com/example/demo/domain/entity/error/ApiExceptionHandler.java
@@ -1,9 +1,28 @@
 package com.example.demo.domain.entity.error;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+	private ApiError createApiError(Exception e) {
+		ApiError apiError = new ApiError();
+		apiError.setMessage(e.getMessage());
+		apiError.setDocumentationUrl("http://example.com/api/errors");
+		return apiError;
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleExceptionInternal(Exception e, Object body, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
+
+		ApiError apiError = createApiError(e);
+		return super.handleExceptionInternal(e, apiError, headers, status, request);
+	}
 
 }
