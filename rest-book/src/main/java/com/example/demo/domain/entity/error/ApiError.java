3c17cce04630312b4804353b375f174cde9d5c40
sc30gsw <haidoushi875@gmail.com>
2022-02-23 13:10:26 +0900

    エラーの詳細情報保持クラスの作成

--- rest-book/src/main/java/com/example/demo/domain/entity/error/ApiError.java
+++ rest-book/src/main/java/com/example/demo/domain/entity/error/ApiError.java
@@ -1,13 +1,29 @@
 package com.example.demo.domain.entity.error;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
 
 @Data
 public class ApiError implements Serializable {
+	
+	@Data
+	private static class Detail implements Serializable {
+		
+		private static final long serialVersionUID = 1L;
+		
+		private final String target;
+		private final String message;
+		private Detail(String target, String message) {
+			this.target = target;
+			this.message = message;
+		}
+	}
 
 	private static final long serialVersionUID = 1L;
 	
@@ -15,4 +31,15 @@ public class ApiError implements Serializable {
 	
 	@JsonProperty("documentaion_url")
 	private String documentationUrl;
+	
+	@JsonInclude(JsonInclude.Include.NON_EMPTY)
+	private final List<Detail> details = new ArrayList<>();
+	
+	public void addDetail(String target, String message) {
+		details.add(new Detail(target, message));
+	}
+	
+	public List<Detail> getDetails() {
+		return details;
+	}
 }
