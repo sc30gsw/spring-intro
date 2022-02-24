8a576b8a5f9acbf6b0a322544a217c3819f4e12f
sc30gsw <haidoushi875@gmail.com>
2022-02-25 08:31:04 +0900

    URIテンプレートの利用

--- rest-client-boook/src/main/java/com/example/demo/RestClientBoookApplication.java
+++ rest-client-boook/src/main/java/com/example/demo/RestClientBoookApplication.java
@@ -1,17 +1,15 @@
 package com.example.demo;
 
-import java.net.URI;
-import java.time.LocalDate;
+import java.util.UUID;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.RequestEntity;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.example.demo.domain.entity.BookResource;
 
@@ -20,6 +18,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @SpringBootApplication
 public class RestClientBoookApplication {
+
+	private static final int T = 0;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestClientBoookApplication.class, args);
@@ -33,21 +33,22 @@ public class RestClientBoookApplication {
 	@Bean
 	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
 		return args -> {
-			BookResource resource = new BookResource();
-			resource.setName("Spring徹底入門");
-			resource.setPublishedDate(LocalDate.of(2016, 4, 1));
-			URI createdResourceUri = restTemplate.postForLocation("http://localhost:8080/books", resource);
+			String bookId = "00000000-0000-0000-0000-000000000000";
 			
-			ResponseEntity<Void> responseEntity = restTemplate.postForEntity("http://localhost:8080/books", resource, Void.class);
+			BookResource resource = restTemplate.getForObject("http://localhost:8080/books/{bookId}", BookResource.class, bookId);
 			
-			HttpStatus httpStatus = responseEntity.getStatusCode();
-			HttpHeaders responseHeaders = responseEntity.getHeaders();
+			RequestEntity<Void> requestEntity = (RequestEntity<Void>) RequestEntity
+					.get(UriComponentsBuilder
+							.fromUriString("http://localhost:8080/books/{bookId}")
+							.buildAndExpand(bookId)
+							.encode()
+							.toUri()
+							)
+					.header("X-Track-Id", UUID.randomUUID().toString())
+					.build();
 			
-			log.info(createdResourceUri.toString());
-			log.info(responseEntity.toString());
-			log.info(httpStatus.toString());
-			log.info(responseHeaders.toString());
-			log.info(resource.getName());
+			log.info(resource.toString());
+			log.info(requestEntity.toString());
 		};
 	}
 
