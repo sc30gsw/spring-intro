7a771460786440a470d2c1bdebbaace2576c4626
sc30gsw <haidoushi875@gmail.com>
2022-02-25 08:18:29 +0900

    リクエストヘッダーの設定

--- rest-client-boook/src/main/java/com/example/demo/RestClientBoookApplication.java
+++ rest-client-boook/src/main/java/com/example/demo/RestClientBoookApplication.java
@@ -2,12 +2,16 @@ package com.example.demo;
 
 import java.net.URI;
 import java.time.LocalDate;
+import java.util.UUID;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import com.example.demo.domain.entity.BookResource;
@@ -34,7 +38,17 @@ public class RestClientBoookApplication {
 			resource.setName("Spring徹底入門");
 			resource.setPublishedDate(LocalDate.of(2016, 4, 1));
 			URI createdResourceUri = restTemplate.postForLocation("http://localhost:8080/books", resource);
+			
+			RequestEntity<BookResource> requestEntity = (RequestEntity<BookResource>) RequestEntity
+					.post(URI.create("http://localhost:8080/books"))
+					.contentType(MediaType.APPLICATION_JSON)
+					.header("X-Track-Id", UUID.randomUUID().toString())
+					.body(resource);
+			
+			ResponseEntity<Void> responseEntity = restTemplate.exchange(requestEntity, Void.class);
 			log.info(createdResourceUri.toString());
+			log.info(requestEntity.toString());
+			log.info(responseEntity.toString());
 			log.info(resource.getName());
 		};
 	}
