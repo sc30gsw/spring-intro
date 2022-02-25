4f22ec9c23aa80b34224bbe1b81a5c462456e64d
sc30gsw <haidoushi875@gmail.com>
2022-02-26 08:09:50 +0900

    オブジェクトをHTTPセッションで管理するための実装

---
+++ echo-app/src/main/java/com/example/demo/app/controller/AccountCreateController.java
@@ -0,0 +1,36 @@
+package com.example.demo.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.example.demo.domain.form.AccountCreateForm;
+
+@Controller
+@RequestMapping("/accounts")
+@SessionAttributes(types = AccountCreateForm.class)
+public class AccountCreateController {
+
+	@ModelAttribute(value = "accountCreateForm")
+	public AccountCreateForm setUpAccountCreateForm() {
+		return new AccountCreateForm();
+	}
+
+	@PostMapping("/create")
+	public String create(@Validated AccountCreateForm form, BindingResult result, @ModelAttribute("password") String password,
+			RedirectAttributes rediretAttributes) {
+		
+		if (result.hasErrors()) {
+			return "redirect:/accounts";
+		}
+		
+		return "redirect:/accounts/create?complete";
+
+	}
+
+}
---
+++ echo-app/src/main/java/com/example/demo/domain/form/AccountCreateForm.java
@@ -0,0 +1,11 @@
+package com.example.demo.domain.form;
+
+import lombok.Data;
+
+@Data
+public class AccountCreateForm {
+
+	private String name;
+	
+	private String password;
+}
