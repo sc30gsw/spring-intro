9f6d4c2324d7754bb61e7adc79684cadbef1a748
sc30gsw <haidoushi875@gmail.com>
2022-02-26 08:13:35 +0900

    バリデーションの追加

--- echo-app/src/main/java/com/example/demo/domain/form/AccountCreateForm.java
+++ echo-app/src/main/java/com/example/demo/domain/form/AccountCreateForm.java
@@ -1,11 +1,18 @@
 package com.example.demo.domain.form;
 
+import java.io.Serializable;
+
+import javax.validation.constraints.NotBlank;
+
 import lombok.Data;
 
 @Data
-public class AccountCreateForm {
+public class AccountCreateForm implements Serializable  {
+	
+	private static final long serialVersionUID = 1L;
 
 	private String name;
 	
+	@NotBlank
 	private String password;
 }
