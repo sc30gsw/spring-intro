ccf04d96d49191320bb7fe48f4537de22899c505
sc30gsw <haidoushi875@gmail.com>
2022-02-26 11:43:53 +0900

    今回の実装には不要のため削除

--- echo-app/src/main/java/com/example/demo/domain/config/WebMvcConfig.java
+++ echo-app/src/main/java/com/example/demo/domain/config/WebMvcConfig.java
@@ -1,11 +1,7 @@
 package com.example.demo.domain.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.AsyncTaskExecutor;
-import org.springframework.core.task.TaskExecutor;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -18,20 +14,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	public void coonfigureAsyncSupport(AsyncSupportConfigurer configurer) {
 		
 		configurer.setDefaultTimeout(5000);
-		
-		//スレッドプールを利用するようにカスタマイズしたTaskExecutorの設定
-		configurer.setTaskExecutor((AsyncTaskExecutor) mvcTaskExecutor());
-		
-	}
-	
-	@Bean
-	public TaskExecutor mvcTaskExecutor() {
-		
-		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-		executor.setCorePoolSize(5);
-		executor.setMaxPoolSize(10);
-		executor.setQueueCapacity(25);
-		return executor;
 		
 	}
 }
