8916876792eb9321098f10aea66b7336fa912a4e
sc30gsw <haidoushi875@gmail.com>
2022-02-26 12:33:43 +0900

    SpringMVCにHandlerMethodArgumentResolverを適用

--- echo-app/src/main/java/com/example/demo/domain/config/WebMvcConfig.java
+++ echo-app/src/main/java/com/example/demo/domain/config/WebMvcConfig.java
@@ -1,10 +1,15 @@
 package com.example.demo.domain.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.example.demo.app.controller.aspect.CommonRequestDataMethodArgumentResolver;
 
 @Configuration
 @EnableWebMvc
@@ -15,5 +20,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
 		
 		configurer.setDefaultTimeout(5000);
 		
+	}
+	
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+		argumentResolvers.add(new CommonRequestDataMethodArgumentResolver());
 	}
 }
