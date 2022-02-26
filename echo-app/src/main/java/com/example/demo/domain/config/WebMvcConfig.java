8a52ea5cfd607a9a0cc8b31aab1ac5e5137f7385
sc30gsw <haidoushi875@gmail.com>
2022-02-26 11:43:02 +0900

    TaskExecutorの設定
    SpringMVC管理のスレッドを使用して非同期処理を実行する場合、要件に合わせて設定が必要。

--- echo-app/src/main/java/com/example/demo/domain/config/WebMvcConfig.java
+++ echo-app/src/main/java/com/example/demo/domain/config/WebMvcConfig.java
@@ -1,7 +1,11 @@
 package com.example.demo.domain.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -14,6 +18,20 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	public void coonfigureAsyncSupport(AsyncSupportConfigurer configurer) {
 		
 		configurer.setDefaultTimeout(5000);
+		
+		//スレッドプールを利用するようにカスタマイズしたTaskExecutorの設定
+		configurer.setTaskExecutor((AsyncTaskExecutor) mvcTaskExecutor());
+		
+	}
+	
+	@Bean
+	public TaskExecutor mvcTaskExecutor() {
+		
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(5);
+		executor.setMaxPoolSize(10);
+		executor.setQueueCapacity(25);
+		return executor;
 		
 	}
 }
