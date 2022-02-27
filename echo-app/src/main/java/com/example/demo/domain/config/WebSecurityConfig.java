a83ade8a6f03e5dcdfa73b8026520b1905bcf5ab
sc30gsw <haidoushi875@gmail.com>
2022-02-28 06:30:57 +0900

    フォーム認証のビュー作成

--- echo-app/src/main/java/com/example/demo/domain/config/WebSecurityConfig.java
+++ echo-app/src/main/java/com/example/demo/domain/config/WebSecurityConfig.java
@@ -1,15 +1,28 @@
 package com.example.demo.domain.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
+@Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(WebSecurity web) {
 		web.ignoring()
 				.antMatchers("/resources/**");
+	}
+	
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests()
+				.antMatchers("/login").permitAll()
+				.anyRequest().authenticated();
+		
+		http.formLogin()
+				.loginPage("/login");
 	}
 }
