1b2034e8c0cb01d4d32f075398e38d23510c1775
sc30gsw <haidoushi875@gmail.com>
2022-03-02 08:45:31 +0900

    JPQLの実装例

--- echo-app/src/main/java/com/example/demo/domain/Repository/RoomRepository.java
+++ echo-app/src/main/java/com/example/demo/domain/Repository/RoomRepository.java
@@ -1,32 +1,12 @@
 package com.example.demo.domain.Repository;
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
-import com.example.demo.domain.entity.Equipment;
 import com.example.demo.domain.entity.Room;
 
 @Repository
-public class RoomRepository {
-	
-	@PersistenceContext
-	EntityManager entityManager;
-	
-	@Transactional(readOnly = true)
-	public List<Equipment> getEquipmentsInRoom(Integer roomId) {
-		Room room = entityManager.find(Room.class, roomId);
-		return room.getEquipments();
-	}
-	
-	@Transactional(readOnly = true)
-	public Room getRoomOfEquipment(Integer equipmentId) {
-		Equipment equipment = entityManager.find(Equipment.class, equipmentId);
-		return equipment.getRoom();
-	}
+public interface RoomRepository extends JpaRepository<Room, Integer> {
+
 
 }
